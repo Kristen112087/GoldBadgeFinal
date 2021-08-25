bed66d0626c7bb7a7b9e07449f3226ad34193d17
Kristen <kristengamelin@gmail.com>
2021-08-25 09:30:05 -0400

    started 3rd choice, working on repo, lots of errors, but pushing anyways

---
+++ 03_Badges_ClassLibrary/BadgesRepo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Badges_ClassLibrary
+{
+    public class BadgesRepo
+    {
+        Dictionary<int, List<string>> _employeeBadges = new Dictionary<int, List<string>>();
+
+        public bool CreateNewBadge(int badgeNum)
+        {
+            try
+            {
+                _employeeBadges.Add(badgeNum, new List<string>());
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //public bool AddDoorToBadge(int badgeNum, string door)
+        //{
+        //    try
+        //    {
+        //        _employeeBadges[badgeNum].Add(door);
+        //        return true;
+        //    }
+        //    catch
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        public bool AddDoorsToBadge(int badgeNum, List<string> doors)
+        {
+            try
+            {
+                _employeeBadges[badgeNum] = doors;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateDoorOnBadge(int badgeNum, List<string> doors)
+        {
+            try
+            {
+                _employeeBadges[badgeNum] = doors;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        public bool RemoveDoorFromBadge(int badgeNum, List<string> doors)
+        {
+            try
+            {
+                foreach (List<string> door in doors)
+                {
+                    if (door.Contains door)
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+    }
+}
---
+++ 03_Badges_Tests/BadgeRepoTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _03_Badges_Tests
+{
+    [TestClass]
+    public class BadgeRepoTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
